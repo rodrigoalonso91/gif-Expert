2e68bd7964f57670890825207c9c20d179ce3c2f
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-25 00:01:56 -0300

    Add test to GifGrid

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import { AddCategory } from './AddCategory';
-import { GifGrid } from './GifGrid'         
+import { AddCategory } from './components/AddCategory'	;
+import { GifGrid } from './components/GifGrid'         
 
 export const GifExpertApp = ({defaultCategories = []}) => {
 
---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import {GifGrid} from '../../components/GifGrid'
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Testing on <GifGrid />', () => { 
+
+	const category = 'dogs';
+	const limit = 10;
+
+	const gifs = [{
+		id: 'ABC',
+		title: 'Titulo Gifs',
+		url: 'https://localhost/cualquier/cosa.jpg'
+	},
+	{
+		id: '123',
+		title: 'Titulo Gifs',
+		url: 'https://localhost/cualquier/cosa.jpg'
+	}];
+
+	test('should show <GifGrid /> correctly', () => { 
+
+		useFetchGifs.mockReturnValue({
+			data: gifs,
+			loading: false
+		});
+
+		expect(render(<GifGrid category={category} limit={limit} />)).toMatchSnapshot()
+	});
+
+	test('should to hide loading message', () => { 
+
+		useFetchGifs.mockReturnValue({
+			data: gifs,
+			loading: false
+		});
+
+		render(<GifGrid category={category} limit={limit} />)
+
+		expect(screen.queryByText('Loading...')).toBeNull()
+	});
+
+	test('should to render all gifts', () => { 
+
+		useFetchGifs.mockReturnValue({
+			data: gifs,
+			loading: false
+		});
+
+		render(<GifGrid category={category} limit={limit} />)
+
+		expect(screen.queryByText('Loading...')).toBeNull()
+		expect(screen.getAllByRole('img').length).toBe(gifs.length)
+	});
+
+	test('should to show the category', () => {
+
+		useFetchGifs.mockReturnValue({
+			data: gifs,
+			loading: false
+		});
+
+		render(<GifGrid category={category} limit={limit} />)
+		expect(screen.getByText(category)).toBeTruthy();
+	})
+})
