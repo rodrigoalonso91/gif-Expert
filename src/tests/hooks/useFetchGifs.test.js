bfe00cfa89b01e08a95ebe31b024d0f958ded7e2
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-26 23:41:17 -0300

    Add basic test

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,13 +1,27 @@
-import { renderHook } from "@testing-library/react"
+import { renderHook, waitFor } from "@testing-library/react"
 import { useFetchGifs } from "../../hooks/useFetchGifs"
 
 describe('Testing on useFetchGifs', () => { 
 	
 	test('should return initial state', () => {
 
-		const { result } = renderHook( () => useFetchGifs( 'Game of thrones', 5 ) )
-		
-		console.log(result)
+		const { result } = renderHook( () => useFetchGifs( 'Game of thrones', 5 ) );
 
+		const { data, loading } = result.current;
+
+		expect( data.length ).toBe(0);
+		expect( loading ).toBeTruthy();
+	});
+
+	test('should return an array of images and loading in false', async () => { 
+
+		const { result } = renderHook( () => useFetchGifs( 'Game of thrones', 5 ) );
+
+		await waitFor( () => expect( result.current.data.length ).toBeGreaterThan(0), {timeout:2000} );
+		
+		const { data, loading } = result.current;
+
+		expect( data.length ).toBeGreaterThan(0);
+		expect( loading ).toBeFalsy();
 	})
 })
